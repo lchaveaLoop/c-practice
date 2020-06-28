b7b412bb467a78ed0b05ff938cc6b85fbcc9081a
lchaveaLoop <lcxiaochang@gmail.com>
2020-06-28 18:46:03 +0800

    not c file

--- 练习/exercise1.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow import keras
-model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd', loss='mean_squared_error')
-xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
-ys = np.array([50000, 100000, 150000, 200000, 250000, 300000], dtype=float)
-model.fit(xs, ys, epochs=10)
-print(model.predict([7.0]))
